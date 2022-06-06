c1f10a860083a7aba26f8d11d7c891014aed4a8f
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-06-07 00:01:08 +0300

    Adds a test the compares two slices with reflect deep equal

--- first-project/arrayslices/array.go
+++ first-project/arrayslices/array.go
@@ -1,5 +1,8 @@
 package arrayslices
 
+// here numbers is actually a slice
+// arrays have their size embedded in the type
+// [5]int is a different type from [4]int
 func Sum(numbers []int) int {
 	result := 0
 
@@ -8,6 +11,16 @@ func Sum(numbers []int) int {
 	// index, value
 	for _, val := range numbers {
 		result += val
+	}
+
+	return result
+}
+
+func SumAll(args ...[]int) []int {
+	result := []int{}
+
+	for _, list := range args {
+		result = append(result, Sum(list))
 	}
 
 	return result
--- first-project/arrayslices/array_test.go
+++ first-project/arrayslices/array_test.go
@@ -1,6 +1,9 @@
 package arrayslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	type args struct {
@@ -40,6 +43,37 @@ func TestSum(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Sum(tt.args.numbers); got != tt.want {
 				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	type args struct {
+		args [][]int
+	}
+	type testCase struct {
+		name string
+		args args
+		want []int
+	}
+
+	tests := []testCase{
+		{
+			name: "should sum each list and return the result",
+			args: args{
+				args: [][]int{
+					{2, 3},
+					{4, 7},
+				},
+			},
+			want: []int{5, 11},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SumAll(tt.args.args...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SumAll() = %v, want %v", got, tt.want)
 			}
 		})
 	}
